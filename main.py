abba32ab488b9cb9b834369291001e04f8ad0934
willson0v0 <willsonlgz@gmail.com>
2022-02-27 23:29:38 +0800

    Updated to latest selenium usage

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ import socket
 import logging
 import sys
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 import time
+from datetime import datetime
+import getpass
+import os
 
 target_ip = "10.248.98.2"
 logging.basicConfig(format="[%(asctime)15s] [%(levelname)8s] %(message)s", stream=sys.stdout, level=logging.INFO, )
 
+WINDOW_SIZE = "1920,1080"
+
+def script_path():
+    return os.path.dirname(os.path.realpath(sys.argv[0]))
 
 def resolve_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -16,11 +25,10 @@ def resolve_ip():
     s.close()
     return ip
 
-
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser(description="input your username and password.")
-    parser.add_argument('--username', type=str, required=True, help='Your user name to login into web auth system.')
-    parser.add_argument('--password', type=str, required=True, help='Your password to login into web auth system.')
+    parser.add_argument('--username', type=str, required=False, help='Your user name to login into web auth system.')
+    parser.add_argument('--password', type=str, required=False, help='Your password to login into web auth system.')
     parser.add_argument('--user-ip', type=str, required=False, help='Your IP address.')
     parser.add_argument('--driver-path', type=str, required=False, help='Where is chromedriver?')
 
@@ -30,17 +38,44 @@ if __name__ == "__main__":
     args.domain = ""
     args.host = "http://" + target_ip + "/srun_portal_pc?ac_id=1&theme=basic2"
     logging.info("HITSZ Fake Web Login V0.1")
+    
+    if args.username is None:
+        args.username = input("Username: ")
+    
+    if args.password is None:
+        args.password = getpass.getpass("Password (no echo): ")
+
     logging.info("Trying to login with " + args.username + " for " + args.user_ip)
     
-    if args.chromedriver_path is None:
-        browser = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+    
+    if args.driver_path is None:
+        browser = webdriver.Chrome(service=Service("dependencies\chromedriver.exe"), options=chrome_options)
     else:
-        browser = webdriver.Chrome(executable_path=args.chromedriver_path)
+        browser = webdriver.Chrome(service=Service(args.driver_path), options=chrome_options)
     
     browser.get(args.host)
     browser.execute_script(f'$(username).val("{args.username}")')
     browser.execute_script(f'$(password).val("{args.password}")')
     browser.execute_script('$(login).click()')
     time.sleep(5)
-    browser.save_screenshot("result.png")
+
+    # create result folder if not there
+    os.makedirs(os.path.join(script_path(), "result"), exist_ok=True)
+
+    time_str = datetime.now().strftime("%Y%m%d%H%M%S")
+    browser.save_screenshot(os.path.join(script_path(), "result", f"result_{time_str}.png"))
+    
+    list_of_files = os.listdir(os.path.join(script_path(), "result"))
+    full_path = [os.path.join(script_path(), "result", x) for x in list_of_files]
+
+    if len(list_of_files) > 5:
+        oldest_file = min(full_path, key=os.path.getctime)
+        os.remove(oldest_file)
+
     logging.info("Login done, please check for result.")
+
+if __name__ == "__main__":
+    main()
